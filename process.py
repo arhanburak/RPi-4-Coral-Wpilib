4f97c83c32982efcf12a2be45e79a5dd3b7b49cf
Arhan Burak Tüzün <64145602+arhanburak@users.noreply.github.com>
2023-10-03 01:05:57 +0300

    add the main python script for image processing

---
+++ process.py
@@ -0,0 +1,75 @@
+# Import the camera server
+from cscore import CameraServer
+
+# Import OpenCV and NumPy
+import cv2
+import numpy as np
+
+def main():
+    model = "model.tflite"
+    labels = "labels.txt"
+
+    cs = CameraServer
+    cs.enableLogging()
+
+    # Capture from the first USB Camera on the system
+    camera = cs.startAutomaticCapture()
+    camera.setResolution(640, 480)
+
+    # Get a CvSink. This will capture images from the camera
+    cvSink = cs.getVideo()
+
+    # (optional) Setup a CvSource. This will send images back to the Dashboard
+    outputStream = cs.putVideo("Processed", 640, 480)
+
+    # Allocating new images is very expensive, always try to preallocate
+    img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
+
+    while True:
+        # Tell the CvSink to grab a frame from the camera and put it
+        # in the source image.  If there is an error notify the output.
+        time, img = cvSink.grabFrame(img)
+        if time == 0:
+            # Send the output the error.
+            outputStream.notifyError(cvSink.getError());
+            # skip the rest of the current iteration
+            continue
+
+        #
+        # Insert your image processing logic here!
+        #
+
+        print('Loading {} with {} labels.'.format(model, labels))
+        interpreter = make_interpreter(model)
+        interpreter.allocate_tensors()
+        labels = read_label_file(labels)
+        inference_size = input_size(interpreter)
+        
+        cv2_im = img
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        cv2_im_rgb = cv2.resize(cv2_im_rgb, inference_size)
+        run_inference(interpreter, cv2_im_rgb.tobytes())
+        objs = get_objects(interpreter, args.threshold)[:args.top_k]
+        cv2_im = append_objs_to_img(cv2_im, inference_size, objs, labels)
+
+        # (optional) send some image back to the dashboard
+        outputStream.putFrame(img)
+
+def append_objs_to_img(cv2_im, inference_size, objs, labels):
+    height, width, channels = cv2_im.shape
+    scale_x, scale_y = width / inference_size[0], height / inference_size[1]
+    for obj in objs:
+        bbox = obj.bbox.scale(scale_x, scale_y)
+        x0, y0 = int(bbox.xmin), int(bbox.ymin)
+        x1, y1 = int(bbox.xmax), int(bbox.ymax)
+
+        percent = int(100 * obj.score)
+        label = '{}% {}'.format(percent, labels.get(obj.id, obj.id))
+
+        cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+        cv2_im = cv2.putText(cv2_im, label, (x0, y0+30),
+        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 0, 0), 2)
+    return cv2_im
+
+if __name__ == '__main__':
+    main()
